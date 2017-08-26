964e4f4eae5ef25a1edd05e3ffd6c24f3d89b534
Kpono-Abasi Akpabio <kingkpono@gmail.com>
2017-08-26 20:24:42 +0100

    Update Data.php
    Added missing quotes

--- app/code/local/Sendbox/Shipments/Helper/Data.php
+++ app/code/local/Sendbox/Shipments/Helper/Data.php
@@ -232,6 +232,110 @@ $origin_state=Mage::getStoreConfig('sendbox_shipments/shipment/origin_state');
 
 
 
+public function build_quote_payload($quote)
+  {
+
+     
+      //billing street
+        $street=Mage::getStoreConfig('sendbox_shipments/shipment/origin_street');
+        
+
+
+        //shipping street
+        $shipping_street="";
+        $i=0;
+        $shipping_str_length=count($quote->getShippingAddress()->getStreet());
+        foreach ($quote->getShippingAddress()->getStreet() as $str) {
+
+          if($i != ($shipping_str_length-1))
+          $shipping_street.=$str.",";
+          else
+           $shipping_street.=$str;
+
+          $i++;
+        }
+        //items
+        $items_string='[';
+        $item_length=count($quote->getAllItems());
+        $j=0;
+      
+       
+        foreach ($quote->getAllItems() as $item) 
+         {
+           
+            $output='{"name": "'.$item->getName().'",
+            "weight": '.$item->getWeight().',
+            "package_size_code": "medium",
+            "quantity": '.$item->getQtyOrdered().',
+            "value": '.$item->getPrice().',
+            "amount_to_receive": '.$item->getPrice().'
+           }';
+
+            if($j != ($item_length-1))
+              $output.=',';
+            $j++;
+           $items_string.=$output;
+         }//end for each item
+
+         $items_string=$items_string.',{"name": "delivery fee",
+            "weight": 0,
+            "package_size_code": "medium",
+            "quantity": 1,
+            "value": '.$quote->getShippingAmount().',
+             "reference_code": "'.$quote->getIncrementId().'",
+            "amount_to_receive": '.$quote->getShippingAmount().'
+           }';
+
+
+         $items_string.=']';
+
+  $billingCountry = Mage::getModel('directory/country')->loadByCode(Mage::getStoreConfig('sendbox_shipments/shipment/origin_country'));
+$shippingCountry = Mage::getModel('directory/country')->loadByCode($quote->getShippingAddress()->getCountry());
+$nextday=$date = new DateTime($quote->getCreatedAt());
+$date->modify('+1 day');
+$pickup_date= $date->format(DateTime::ATOM); 
+  $origin_name=Mage::getStoreConfig('sendbox_shipments/shipment/origin_name');
+$origin_phone=Mage::getStoreConfig('sendbox_shipments/shipment/origin_phone');
+$origin_street=Mage::getStoreConfig('sendbox_shipments/shipment/origin_street');
+$origin_city=Mage::getStoreConfig('sendbox_shipments/shipment/origin_city');
+$origin_address=Mage::getStoreConfig('sendbox_shipments/shipment/origin_address');
+$origin_state=Mage::getStoreConfig('sendbox_shipments/shipment/origin_state');
+    $payload=' {
+  "origin_name": "'.$origin_name.'",
+  "origin_address": "'.$street.'",
+  "origin_phone": "'.$origin_phone.'",
+  "origin_street":  "'.$street.'",
+  "origin_city": "'.$origin_city.'",
+  "origin_state":"'.$origin_state.'",
+  "origin_country": "'. $billingCountry->getName().'",
+  
+  "destination_name": "'.$quote->getShippingAddress()->getName().'",
+  "destination_address": "'.$shipping_street.'",
+  "destination_phone":  "'.$quote->getShippingAddress()->getTelephone().'",
+  "destination_street":  "'.$shipping_street.'",
+  "destination_city": "'.$quote->getShippingAddress()->getCity().'",
+  "destination_state": "'.$quote->getShippingAddress()->getRegion().'",
+  "destination_country": "'.$shippingCountry->getName().'",
+
+  "delivery_priority_code": "next_day",
+  
+  "incoming_option_code": "pickup",
+  "pickup_date":"'.$pickup_date.'",
+  "delivery_type_code": "last_mile",
+  
+  "accept_value_on_delivery": true,
+  "amount_to_receive": '.($quote->getSubtotal()+$fee).',
+  "fee_payment_channel_code": "cash",
+  "channel_code": "website",  
+    "items": '.$items_string.'
+ 
+}';
+
+     return $payload;
+       
+  }
+
+
  
 
 
